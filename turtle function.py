2528b85bb75d15affd1bdb4573a69ff53d1cea1e
132-mzylka <115180439+132-mzylka@users.noreply.github.com>
2022-10-06 10:46:31 -0700

    Add files via upload

---
+++ turtle function.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+
+import turtle as tur
+
+ws = tur.Turtle()
+ 
+
+ 
+tur.pensize(4)
+ 
+
+tur.penup()
+tur.setpos(-90,30)
+tur.pendown()
+for i in range(5):
+    tur.forward(200)
+    tur.right(144)
+ 
+tur.penup()
+tur.setpos(80,-140)
+tur.pendown()
+ 
+
+tur.pencolor("Black")
+tur.done()
