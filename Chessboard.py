ad33e08130c7c1c8b4cad035cddcb4d36df74dba
132-mzylka <115180439+132-mzylka@users.noreply.github.com>
2022-10-06 10:35:41 -0700

    Add files via upload

---
+++ Chessboard.py
@@ -0,0 +1,352 @@
+import turtle 
+x=-300
+y=-300
+turtle.speed(15)
+
+def squarzy(x,y):
+    turtle.pu()
+    turtle.goto(x, y)
+    turtle.pd()
+    for i in range (4):
+        turtle.forward(100)
+        turtle.right(90)
+while x< 500:
+    squarzy(x, y)
+    x+=100
+    print(x)
+    if x == 500:
+        x = -300
+        y +=100
+    if y == 500:
+        break
+
+turtle.fillcolor("black")
+turtle.begin_fill()
+  
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+  
+turtle.backward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill() 
+
+
+turtle.backward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.backward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill() 
+
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.left(180)
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.left(180)
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+turtle.forward(200)
+turtle.left(180)
+
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
+
+turtle.forward(200)
+turtle.begin_fill()
+
+for _ in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+turtle.end_fill()
+
